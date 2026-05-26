252c5a0f33f4fefa2c8dc09d394a957b6003386c
tremran <tremran@gmail.com>
2026-05-26 10:50:41 +0200

    making User model available for Spring Security

--- src/main/java/com/app/estate_api/model/User.java
+++ src/main/java/com/app/estate_api/model/User.java
@@ -1,9 +1,13 @@
 package com.app.estate_api.model;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -16,7 +20,7 @@ import lombok.Data;
 @Table(name="USERS")
 @Entity
 @Data
-public class User {
+public class User implements UserDetails{
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(nullable = false)
@@ -38,4 +42,14 @@ public class User {
     @UpdateTimestamp
     @Column(name = "updated_at")
     private Date updatedAt;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
 }
